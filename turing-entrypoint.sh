d51a0c69919fe0185db50d574dc7b194df8f9c3c
Alexey Borovkov <lucky.aaz@gmail.com>
2019-07-10 18:54:02 +0300

    docker is fine

--- turing-entrypoint.sh
+++ turing-entrypoint.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
-cd /var/www/laravel;
+set -x;
+
+/bin/bash /entrypoint.sh mysqld > /dev/null 2>&1 &
+
+sudo -uwww-data composer install;
+cp .env.example .env;
 php artisan serve --host=0.0.0.0;
 
